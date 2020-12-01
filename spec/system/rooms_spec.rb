4388eecc9868936f7984f7e27587c7bb27a377b4
takumi-murakami <takumi28280503@gmail.com>
2020-12-01 09:31:37 +0900

    commit

--- spec/system/rooms_spec.rb
+++ spec/system/rooms_spec.rb
@@ -18,8 +18,8 @@ RSpec.describe "Rooms", type: :system do
     # 「チャットを終了する」ボタンをクリックすることで、作成した5つのメッセージが削除されていることを確認する
     expect{
       find_link('チャットを終了する', href: room_path(@room_user.room)).click
-    }.to change{ Message.count }.by(-5)
-  
+    }.to change{ @room_user.room.messages.count }.by(-5)
+
     # トップページに遷移していることを確認する
     expect(current_path).to eq root_path
   end
